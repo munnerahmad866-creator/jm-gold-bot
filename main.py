24f528e7c94d3222d34a8b9d74a62df49251bb2f
munnerahmad866-creator <munnerahmad866@gmail.com>
2026-09-17 17:04:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "أهلاً شيخ منير البوت شغال 24 ساعة 🔥")
+    bot.reply_to(message, "شغال 24 ساعة ✅")
 
 @app.route('/')
 def home():
-    return "Bot is Running 24/7 - @munner_er_2026_bot"
+    return "Bot is Running..."
 
 def run_bot():
     print("Bot polling started...")
     bot.infinity_polling()
 
 def run_flask():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
 if __name__ == "__main__":
     t = threading.Thread(target=run_bot)
